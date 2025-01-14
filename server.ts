9896e33f0d70e239d49abf148ecf3596e588990f
razmiO <thomas.cuesta31@gmail.com>
2025-01-14 19:21:07 +0100

    Refactor server.ts to improve code organization and readability

--- server.ts
+++ server.ts
@@ -3,15 +3,16 @@ import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
 import { logger } from "hono/logger";
 
-const app = new Hono().use(logger());
-app.route("/", pagesRoutes);
-
-/* static files served at /public */
-app.get(
-    "/*",
-    serveStatic({
-        root: "./public",
-    })
-);
+const app = new Hono()
+    .use(logger())
+    /* routes for pages */
+    .route("/", pagesRoutes)
+    /* static files served at /public */
+    .get(
+        "/*",
+        serveStatic({
+            root: "./public",
+        })
+    );
 
 Deno.serve({ port: 5050 }, app.fetch);
