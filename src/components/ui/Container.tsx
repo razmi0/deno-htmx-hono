1743978b8a08c5420bed15447d50c5b9757745b2
razmiO <thomas.cuesta31@gmail.com>
2025-01-16 16:17:53 +0100

    feat(ui): Add Container component for wrapping content

---
+++ src/components/ui/Container.tsx
@@ -0,0 +1,3 @@
+export const Container = ({ children }: { children: any }) => {
+    return <section class="rounded-lg ring-1 ring-stone-400 py-2 px-4 bg-stone-700 space-y-1">{children}</section>;
+};
