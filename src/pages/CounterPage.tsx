adebf2fc70b041ed105d8529fabbe3935079e730
razmiO <thomas.cuesta31@gmail.com>
2025-01-16 18:52:44 +0100

    refactor(ui): Update CounterPage component imports

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,29 +1,27 @@
 import Counter from "@components/Counter.tsx";
-import { Header } from "@components/Header.tsx";
+import { Header } from "../components/ui/Header.tsx";
+import { Layout } from "@pages/Layout.tsx";
 import { Hono } from "hono";
-import { Fragment } from "hono/jsx";
 
 /* base path : /counter */
-export const counterRoutes = new Hono();
-
-counterRoutes.get(":add", (c) => {
-    const add = parseInt(c.req.param("add"));
-    return c.render(<Counter add={add} hxPath="counter" id="counter" />);
-});
-
-export const CounterPage = ({ bigTitle }: { bigTitle: string }) => {
-    return (
-        <Fragment>
-            <Header />
-            <main>
-                <h1>{bigTitle}</h1>
-                <p>
-                    This counter is implemented using htmx. A click call a endpoint to the server with the add value (1
-                    or -1) and return server side JSX rendered HTML replacing the counter with the updated value. The
-                    state of the counter is holded on the server.
-                </p>
-                <Counter id="counter" hxPath="counter" />
-            </main>
-        </Fragment>
-    );
-};
+export default new Hono()
+    .get("/", (c) =>
+        c.render(
+            <Layout title="Counter">
+                <Header />
+                <main>
+                    <h1>Counter</h1>
+                    <p>
+                        This counter is implemented using htmx. A click call a endpoint to the server with the add value
+                        (1 or -1) and return server side JSX rendered HTML replacing the counter with the updated value.
+                        The state of the counter is holded on the server.
+                    </p>
+                    <Counter id="counter" hxPath="counter" />
+                </main>
+            </Layout>
+        )
+    )
+    .get(":add", (c) => {
+        const add = parseInt(c.req.param("add"));
+        return c.render(<Counter add={add} hxPath="counter" id="counter" />);
+    });
