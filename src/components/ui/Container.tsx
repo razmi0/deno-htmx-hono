e499eec1b2632d0ecb9ac6251e77f5a89b7b8bf8
razmiO <thomas.cuesta31@gmail.com>
2025-01-16 16:49:50 +0100

    refactor(ui): Update Container component to accept an optional id prop

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,3 +1,9 @@
-export const Container = ({ children }: { children: any }) => {
-    return <section class="rounded-lg ring-1 ring-stone-400 py-2 px-4 bg-stone-700 space-y-1">{children}</section>;
+export const Container = ({ children, id }: { children: any; id?: string | number }) => {
+    return (
+        <section
+            id={id ? String(id) : undefined}
+            class="rounded-lg ring-1 ring-stone-400 py-2 px-4 bg-stone-700 space-y-1 shadow-lg shadow-black">
+            {children}
+        </section>
+    );
 };
