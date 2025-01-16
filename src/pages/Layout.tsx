1b44ec45be9b2cd46e7afd7e2fcd53ab2e65d48e
razmiO <thomas.cuesta31@gmail.com>
2025-01-16 18:51:54 +0100

    chore(ui): mv

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,18 @@
+export const Layout = ({ children, title }: { children: any; title: string }) => {
+    return (
+        <>
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="stylesheet" href="/index.css" />
+                <script type="module" src="/index.js"></script>
+                <script type="module" src="/libs/htmx.js"></script>
+                <title>{title}</title>
+            </head>
+            <body>
+                {children}
+                <footer></footer>
+            </body>
+        </>
+    );
+};
