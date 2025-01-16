eb5fcaee611f2ea1707d7f4a9b9c4337c51711fd
razmiO <thomas.cuesta31@gmail.com>
2025-01-16 16:27:34 +0100

    refactor(todopage) : route page declaration and todo business close to each other

--- src/pages/routes/pageRoutes.tsx
+++ src/pages/routes/pageRoutes.tsx
@@ -1,32 +1,25 @@
 import { CounterPage, counterRoutes } from "@pages/CounterPage.tsx";
 import { HomePage } from "@pages/HomePage.tsx";
 import { Layout } from "@pages/layout/Layout.tsx";
-import todosRoutes, { TodosPage } from "@pages/TodosPage.tsx";
+import todosRoutes from "@pages/TodosPage.tsx";
 import { Hono } from "hono";
 import { jsxRenderer } from "hono/jsx-renderer";
 
-const Home = () => (
-    <Layout title="Home">
-        <HomePage bigTitle="Home" />
-    </Layout>
-);
-
-const Counter = () => (
-    <Layout title="Counter">
-        <CounterPage bigTitle="Counter" />
-    </Layout>
-);
-
-const Todos = () => (
-    <Layout title="Todos">
-        <TodosPage bigTitle="Todos" />
-    </Layout>
-);
-
 export default new Hono()
     .route("/counter", counterRoutes)
-    .route("/", todosRoutes)
+    .route("/todos", todosRoutes)
     .get("/*", jsxRenderer())
-    .get("/", (c) => c.render(<Home />))
-    .get("/counter", (c) => c.render(<Counter />))
-    .get("/todos", (c) => c.render(<Todos />));
+    .get("/", (c) =>
+        c.render(
+            <Layout title="Home">
+                <HomePage bigTitle="Home" />
+            </Layout>
+        )
+    )
+    .get("/counter", (c) =>
+        c.render(
+            <Layout title="Counter">
+                <CounterPage bigTitle="Counter" />
+            </Layout>
+        )
+    );
