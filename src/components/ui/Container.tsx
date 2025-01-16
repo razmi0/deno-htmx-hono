a5e3ddc26742ddebb95888eb5dd2410a73b47289
razmiO <thomas.cuesta31@gmail.com>
2025-01-16 18:51:16 +0100

    styles(ui): Update Container

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,9 +1,9 @@
 export const Container = ({ children, id }: { children: any; id?: string | number }) => {
     return (
-        <section
+        <article
             id={id ? String(id) : undefined}
-            class="rounded-lg ring-1 ring-stone-400 py-2 px-4 bg-stone-700 space-y-1 shadow-lg shadow-black">
+            class="ring-1 ring-stone-400 py-2 px-4 bg-stone-700 space-y-1 shadow-lg shadow-black">
             {children}
-        </section>
+        </article>
     );
 };
