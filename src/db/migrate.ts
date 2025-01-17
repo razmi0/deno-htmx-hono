7e4377bdfcbe2ba58808a8d502843d8894136141
razmiO <thomas.cuesta31@gmail.com>
2025-01-17 01:58:00 +0100

    refactor(db): ensure file exist todos.json

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,8 +1,17 @@
 import db from "@/db/index.ts";
+import { ensureFileSync } from "jsr:@std/fs";
 
+// Synchronously ensures that the file exists.
+// If the file already exists, this function does nothing. If the parent directories for the file do not exist, they are created.
+ensureFileSync(db.JSON_PATH);
+
+// reset the todos
 const askReset = Deno.args[0] === "--reset";
+
+// limit the number of todos generated
 const limit = Deno.args[1] ? parseInt(Deno.args[1]) : 10;
 
+// reset the todos
 if (askReset) {
     const todos = await db.readTodos();
     if (!todos) throw new Error("[RESETING] : Failed to read todos in migration !");
@@ -14,6 +23,7 @@ if (askReset) {
     console.log("[RESETING] : Done !");
 }
 
+// The todo generator function
 function* todoGenerator(): Generator<Todo> {
     const titles = [
         "Learn React",
@@ -37,6 +47,7 @@ function* todoGenerator(): Generator<Todo> {
     }
 }
 
+// Add todos one by one over IteratorObject range of limit
 for (const todo of todoGenerator().take(limit)) {
     const newTodos = await db.addTodo(todo);
     if (!newTodos) throw new Error("Failed to update todo in migration !");
