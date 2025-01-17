2ad1eb3ebee890485219ea0ace6aba60fa3835b0
razmiO <thomas.cuesta31@gmail.com>
2025-01-17 02:02:03 +0100

    refactor(client): switch client dependency to cdn script tag

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -6,7 +6,9 @@ export const Layout = ({ children, title }: { children: any; title: string }) =>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="stylesheet" href="/index.css" />
                 <script type="module" src="/index.js"></script>
-                <script type="module" src="/libs/htmx.js"></script>
+                {/* htmx */}
+                <script src="https://unpkg.com/htmx.org@2.0.4"></script>
+                <script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
                 <title>{title}</title>
             </head>
             <body>
