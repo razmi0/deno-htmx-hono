533c64d5e660ddb2388e88cd5a3884451c9428f8
razmiO <thomas.cuesta31@gmail.com>
2025-01-19 14:24:08 +0100

    refactor: Update Checkbox component to support HTMXProps

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,7 +1,21 @@
-export const Checkbox = ({ checked, name = "completed" }: { checked?: boolean; name?: string }) => {
+export const Checkbox = ({
+    checked,
+    name = "completed",
+    hxProps,
+}: {
+    checked?: boolean;
+    name?: string;
+    hxProps?: HTMXProps;
+}) => {
     return (
         <label class="flex justify-center items-center">
-            <input class="peer cursor-pointer hidden after:opacity-100" type="checkbox" name={name} checked={checked} />
+            <input
+                class="peer cursor-pointer hidden after:opacity-100"
+                type="checkbox"
+                name={name}
+                checked={checked}
+                {...hxProps}
+            />
             <span class="inline-block w-5 h-5 border-2 relative cursor-pointer after:content-[''] after:bg-green-600 after:absolute after:top-2/4 after:left-2/4 after:-translate-x-1/2 after:-translate-y-1/2 after:w-[10px] after:h-[10px] after:rounded-[2px] after:opacity-0 peer-checked:after:opacity-100" />
         </label>
     );
